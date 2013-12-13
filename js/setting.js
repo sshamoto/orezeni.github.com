08091bce6847da44ba818a3b8912dbbd166af076
sshamoto <sshamoto@conly.jp>
2013-12-13 14:06:32 +0900

    correct misspell

--- js/setting.js
+++ js/setting.js
@@ -10,7 +10,7 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'sasgamihara_budget_2013';
+OpenSpending.identifier = 'sagamihara_budget_2013';
 OpenSpending.year = '2013';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
